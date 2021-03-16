99edc04db52317b47ed024966d983e1df311342e
valentinkatanga <valentin.katanga@student.odisee.be>
2021-03-16 16:34:33 +0100

    taken

--- Slnles04/WpfCarConfigurator/MainWindow.xaml.cs
+++ Slnles04/WpfCarConfigurator/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace WpfCarConfigurator
 {
@@ -24,54 +25,76 @@ namespace WpfCarConfigurator
         public MainWindow()
         {
             InitializeComponent();
-            BerekenLayout();
 
         }
-        int prijs = 0;
-        private void BerekenLayout()
+
+        private int prijs;
+        private void radiobutton1_CheckedChanged(Object sender, EventArgs e)
+        { 
+            prijs = Berekenprijs();
+            txtBox.Text = Convert.ToString(prijs);
+            Layout();
+        }
+        private void radiobutton2_CheckedChanged(Object sender, EventArgs e)
         {
-            if (combops1.IsSelected == true)
+            prijs = Berekenprijs();
+            txtBox.Text = Convert.ToString(prijs);
+            Layout();
+        }
+
+        private void radiobutton3_CheckedChanged(Object sender, EventArgs e)
+        {
+            prijs = Berekenprijs();
+            txtBox.Text = Convert.ToString(prijs);
+            Layout();
+        }
+        private void accessoire1(object sender, EventArgs e)
+        {
+            berekenAssecoire();
+        }
+        private void accessoire2(object sender, EventArgs e)
+        {
+            berekenAssecoire();
+        }
+        private void accessoire3(object sender, EventArgs e)
+        {
+            berekenAssecoire();
+            Layout();
+        }
+
+
+    private int Berekenprijs()
+    {
+            int prijs = 0;
+            if (combops1.IsSelected)
             {
                 if (zwart.IsChecked == true)
                 {
-
-                    psimg.Source = new BitmapImage(new Uri("playstation/ps1b.jpg", UriKind.Relative));
                     prijs += 170;
-
                 }
                 else
                 if (wit.IsChecked == true)
                 {
-
-                    psimg.Source = new BitmapImage(new Uri("playstation/ps1w.jpg", UriKind.Relative));
                     prijs += 100;
-
-
                 }
                 else
                 if (grijs.IsChecked == true)
                 {
-
-                    psimg.Source = new BitmapImage(new Uri("playstation/ps1g.jpg", UriKind.Relative));
                     prijs += 125;
-
                 }
             }
             else
-         if (combops2.IsSelected == true)
+        if (combops2.IsSelected == true)
             {
                 if (zwart.IsChecked == true)
                 {
-
-                    psimg.Source = new BitmapImage(new Uri(@"playstation/ps2b.jpg", UriKind.Relative));
-                    prijs += 120 + 70;
-
+                    prijs += 190;
                 }
                 else
                 if (wit.IsChecked == true)
                 {
 
-                    psimg.Source = new BitmapImage(new Uri(@"playstation/ps2w.jpg", UriKind.Relative));
+
                     prijs += 120;
 
                 }
@@ -79,8 +102,8 @@ namespace WpfCarConfigurator
                 if (grijs.IsChecked == true)
                 {
 
-                    psimg.Source = new BitmapImage(new Uri(@"playstation/ps2g.jpg", UriKind.Relative));
-                    prijs += 120 + 25;
+
+                    prijs += 145;
                 }
             }
             else
@@ -89,14 +112,14 @@ namespace WpfCarConfigurator
                 if (zwart.IsChecked == true)
                 {
 
-                    psimg.Source = new BitmapImage(new Uri(@"playstation/ps2b.jpg", UriKind.Relative));
+
                     prijs += 150 + 70;
                 }
                 else
                 if (wit.IsChecked == true)
                 {
 
-                    psimg.Source = new BitmapImage(new Uri(@"playstation/ps2w.jpg", UriKind.Relative));
+
                     prijs += 150;
 
                 }
@@ -104,36 +127,148 @@ namespace WpfCarConfigurator
                 if (grijs.IsChecked == true)
                 {
 
-                    psimg.Source = new BitmapImage(new Uri(@"playstation/ps2g.jpg", UriKind.Relative));
+
                     prijs += 150 + 25;
                 }
+
+
+            }
+            return prijs;
+
+        }
+
+        private void berekenAssecoire()
+
+        {
+            prijs = Berekenprijs();
+            if ((bool)headset.IsChecked)
+            {
+
+                prijs += 40;
+            }
+            else
+            if ((bool)randgame.IsChecked)
+            {
+                prijs += 60;
+            }
+            else
+            if ((bool)dualshock.IsChecked)
+            {
+              
+                prijs += 70;
+            }
+
+            txtBox.Text = Convert.ToString(prijs);
+        }
+    private void Layout()
+    {
+        if (combops1.IsSelected == true)
+        {
+            if (zwart.IsChecked == true)
+            {
+
+                psimg.Source = new BitmapImage(new Uri("playstation/ps1b.jpg", UriKind.Relative));
+
+
+            }
+            else
+            if (wit.IsChecked == true)
+            {
+
+                psimg.Source = new BitmapImage(new Uri("playstation/ps1w.jpg", UriKind.Relative));
+
+
+
+            }
+            else
+            if (grijs.IsChecked == true)
+            {
+
+                psimg.Source = new BitmapImage(new Uri("playstation/ps1g.jpg", UriKind.Relative));
+
+
+            }
+        }
+        else
+        if (combops2.IsSelected == true)
+        {
+            if (zwart.IsChecked == true)
+            {
+
+                psimg.Source = new BitmapImage(new Uri(@"playstation/ps2b.jpg", UriKind.Relative));
+
+
+            }
+            else
+            if (wit.IsChecked == true)
+            {
+
+                psimg.Source = new BitmapImage(new Uri(@"playstation/ps2w.jpg", UriKind.Relative));
+
+
+            }
+            else
+            if (grijs.IsChecked == true)
+            {
+
+                psimg.Source = new BitmapImage(new Uri(@"playstation/ps2g.jpg", UriKind.Relative));
+
+            }
+        }
+        else
+         if (combops3.IsSelected == true)
+         {
+            if (zwart.IsChecked == true)
+            {
+
+                psimg.Source = new BitmapImage(new Uri(@"playstation/ps2b.jpg", UriKind.Relative));
+
+            }
+            else
+            if (wit.IsChecked == true)
+            {
+
+                psimg.Source = new BitmapImage(new Uri(@"playstation/ps2w.jpg", UriKind.Relative));
+
+
+            }
+            else
+            if (grijs.IsChecked == true)
+            {
+
+                psimg.Source = new BitmapImage(new Uri(@"playstation/ps2g.jpg", UriKind.Relative));
+
             }
             if (headset.IsChecked == true)
             {
                 headset.Opacity = 1;
-                prijs += 40;
             }
             else
             if (randgame.IsChecked == true)
             {
                 randgame.Opacity = 1;
-                prijs += 60;
             }
             else
             if (dualshock.IsChecked == true)
             {
                 dualshock.Opacity = 1;
-                prijs += 70;
             }
+         }
+    }
+
+        private void headset_Unchecked(object sender, RoutedEventArgs e)
+        {
+
         }
+    }
+
+
     }
 
 
 
 
 
-
-
-}   
+   
 
 
